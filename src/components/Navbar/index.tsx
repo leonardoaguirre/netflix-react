e1eb3817689491a177d7492e7b3b88cfe50519a5
leonardoaguirre <62442754+leonardoaguirre@users.noreply.github.com>
2022-01-15 18:16:38 -0300

    Refatora navBar responsiva com boostrap

--- src/components/Navbar/index.tsx
+++ src/components/Navbar/index.tsx
@@ -1,18 +1,32 @@
 import React from "react";
+import { Container, Nav, Navbar } from "react-bootstrap";
 import './style.css';
 
-const NavBar = () => {
+const NavBar: React.FC = () => {
     return (
         <>
-            <div className='container'>
-                <h2 className='logo'>LEOFLIX</h2>
-                <nav>
-                    <a href="#">Inicio</a>
-                    <a href="#">Séries</a>
-                    <a href="#">Filmes</a>
-                    <a href="#">Documentários</a>
-                </nav>
-            </div>
+            <Navbar
+                collapseOnSelect
+                expand="lg"
+                bg="dark"
+                variant="dark"
+                className="justify-content-space-between">
+                <Nav>
+                    <Navbar.Brand href="#home">
+                        <h2 className='logo'>LEOFLIX</h2>
+                    </Navbar.Brand>
+                </Nav>
+
+                <Navbar.Toggle aria-controls="responsive-navbar-nav" />
+                <Navbar.Collapse id="responsive-navbar-nav">
+                    <Nav className="justify-content-space-between">
+                        <Nav.Link href="/">Inicio</Nav.Link>
+                        <Nav.Link href="/">Séries</Nav.Link>
+                        <Nav.Link href="/">Filmes</Nav.Link>
+                        <Nav.Link href="/">Documentários</Nav.Link>
+                    </Nav>
+                </Navbar.Collapse>
+            </Navbar>
         </>
     )
 }
